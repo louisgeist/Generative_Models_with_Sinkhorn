6a9098fdbcf5ff20ce4901c1c739a560917a8ef4
Louis Geist <louisjjgeist@gmail.com>
2023-11-26 15:12:19 +0100

    vlisualization - eval model for dropout

--- visualization.py
+++ visualization.py
@@ -11,18 +11,10 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
 
-batch_size = 32
-epochs = 10
-generator_dim = [[2,32], [32,128], [128, 784]] #last one should be [_,784]
-learned_cost_dim = [[784, 128], [128, 128]] #first one should be [784, _]
-lr = 0.01
-learnable_cost = False
-epsilon = 0.1
 
-
-Model = Model(generator_dim, learned_cost_dim, batch_size, lr, epsilon, learnable_cost)
 Model = torch.load('basic_model.pt')
 
+Model.eval()
 sample = Model()
 
 def visualize(sample : torch.Tensor):
